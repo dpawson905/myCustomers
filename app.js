3ddab40b9231c7c317c4c8ebcd267b79b6555bdb
Darrell Pawson <dpawson905@gmail.com>
2018-10-27 15:55:54 -0400

    setting up for production

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
-// app.use(cookieParser());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
