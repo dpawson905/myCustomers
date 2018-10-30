c39b8d2a56c0915102a4781505b76638958c4e62
Darrell Pawson <dpawson905@gmail.com>
2018-10-30 19:09:51 -0400

    finish up comment deletion

--- routes/notes.js
+++ routes/notes.js
@@ -6,26 +6,38 @@ const Note = require('../models/notes');
 const {asyncErrorHandler, isNotAuthenticated} = require('../middleware/index');
 
 router.post("/", isNotAuthenticated, async(req, res) => {
-    let customer = await Customer.findById(req.params.id);
-    if(!customer) {
-        req.flash('error', 'No customer found.');
-        res.redirect('back');
-        return;
+  let customer = await Customer.findById(req.params.id);
+  if(!customer) {
+    req.flash('error', 'No customer found.');
+    res.redirect('back');
+    return;
+  }
+  Note.create(req.body, async (err, note) => {
+    if(err) {
+      req.flash('error', 'Note cannot be created.');
+      res.redirect('back');
+      return;
     }
-    Note.create(req.body, async (err, note) => {
-        if(err) {
-            req.flash('error', 'Note cannot be created.');
-            res.redirect('back');
-            return;
-        }
-        note.author.id = req.user.id;
-        note.author.username = req.user.username;
-        await note.save();
-        await customer.notes.push(note);
-        await customer.save();
-        req.flash('success', 'Comment created');
-        res.redirect('/customers/' + customer.id);
-    })
+    note.author.id = req.user.id;
+    note.author.username = req.user.username;
+    await note.save();
+    await customer.notes.push(note);
+    await customer.save();
+    req.flash('success', 'Comment created');
+    res.redirect('/customers/' + customer.id);
+  })
+})
+
+router.delete("/:notes_id", isNotAuthenticated, async(req, res) => {
+  let customer = await Customer.findByIdAndUpdate(req.params.id, {$pull: {notes: req.params.notes_id}});
+  if (!customer) {
+    req.flash('error', 'No customer found');
+    res.redirect('back');
+    return;
+  }
+  await Note.findByIdAndRemove(req.params.notes_id);
+  req.flash('success', 'Note deleted');
+  res.redirect('back');
 })
 
 module.exports = router;
